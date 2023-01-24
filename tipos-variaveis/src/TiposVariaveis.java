dab46a8a6892344648cd384e3c5114c7ca2b2a6b
Rafaelmantovaneli <rafaelmantovaneli@gmail.com>
2023-01-24 11:02:02 -0300

    projeto SmartTv Metodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        
+        System.out.println((concatenacao));
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println((concatenacao));
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println((concatenacao));
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println((concatenacao));
+
+        concatenacao = "1" + (1 + 1 + 1);
+
     }
 }

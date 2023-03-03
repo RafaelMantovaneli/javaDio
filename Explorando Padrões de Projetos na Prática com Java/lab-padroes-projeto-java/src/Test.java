13db720194ff8ff8aaeb26c01db0a465614abd64
Rafaelmantovaneli <rafaelmantovaneli@gmail.com>
2023-03-03 12:32:53 -0300

    Final Java

--- Explorando Padrões de Projetos na Prática com Java/lab-padroes-projeto-java/src/Test.java
+++ Explorando Padrões de Projetos na Prática com Java/lab-padroes-projeto-java/src/Test.java
@@ -1,5 +1,17 @@
+import Singleton.SingletonEager;
+import Singleton.SingletonLazy;
+import Singleton.SingletonLazyHolder;
+import Strategy.Comportamento;
+import Strategy.ComportamentoAgessivo;
+import Strategy.ComportamentoDefensivo;
+import Strategy.ComportamentoNormal;
+import Strategy.Robo;
+
 public class Test {
     public static void main(String[] args) {
+
+        // Singleton
+
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
@@ -14,5 +26,28 @@ public class Test {
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
+
+        // Strategy
+        
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgessivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();   
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
     }
-}
+    
+    }
+
